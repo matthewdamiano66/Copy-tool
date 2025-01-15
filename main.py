9f37ea270138c2953e1fb41ad7c0810b42f13a6f
Matthew <58225683+matthewdamiano66@users.noreply.github.com>
2025-01-15 15:37:01 -0500

    Update main.py
    Added log creation for history of copies, file path match checking to stop copies to the same directory

--- main.py
+++ main.py
@@ -1,23 +1,60 @@
 import os
-import sys
-
-scanner = input("Please enter source path:")
-source_letter= scanner
-scanner = input("Please enter a destination path:")
-destination_letter = scanner
-source_path = source_letter+":\ "
-destination_path=destination_letter + ":\ "
+import time
 
 
-flags = "/h/e/r/k/y/j"
-
-if source_letter == destination_letter:
-    exit(0)
-else:
-    print("Beginnig Copy...")
-
-command = "xcopy" + " "+ source_path+ " "+ destination_path+" "+flags
+def gui():
+    return
 
 
-print(command)
-#os.system(command)
+def options():
+    local = "local"
+    network = "network"
+    reply = input("Will this copy be local or via network connection?")
+    if reply == local:
+        xcopy()
+    elif reply == network:
+        robocopy()
+    else:
+        print("Please check the available options and try again!")
+        exit(0)
+
+
+def xcopy():
+    scanner = input("Please enter source path:")
+    source_path = str(scanner)
+    scanner = input("Please enter a destination path:")
+    destination_path = str(scanner)
+    flags = "/h/e/r/k/y/j"
+    if source_path.lower() == destination_path.lower():
+        print("Please check entered values and try again!")
+        exit(0)
+    else:
+        print("Starting Copy...")
+    command = "xcopy" + " " + source_path + " " + destination_path + " " + flags
+    print(command)
+    os.system(command)
+    w = open("history.txt", "a")
+    w.write(str(time.ctime()) + " " + command + "\n")
+
+
+def robocopy():
+    start = "Robocopy"
+    flags = "/E /XC /XN /XO"
+    user = str(input("Please enter the source path:")).lower()
+    source_path = user.lower()
+    user = str(input("Please enter the destination path:")).lower()
+    destination_path = user
+    if source_path.lower() == destination_path.lower():
+        print("Please check entered values and try again")
+        exit(0)
+    else:
+        print("Starting Copy...")
+    command = start + " " + source_path + " " + destination_path + " " + flags
+    os.system(command)
+    print(command)
+    w = open("history.txt", "a")
+    w.write(str(time.ctime()) + " " + command + "\n")
+
+
+if __name__ == '__main__':
+    options()
